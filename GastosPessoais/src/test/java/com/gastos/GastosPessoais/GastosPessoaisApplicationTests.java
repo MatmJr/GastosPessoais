ecf170f142b63adca58619f8f76ffae6441e3e11
Marco Mialaret Junior <matmj@cin.ufpe.br>
2023-09-05 12:39:13 -0300

    criação do projeto

---
+++ GastosPessoais/src/test/java/com/gastos/GastosPessoais/GastosPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gastos.GastosPessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GastosPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
